460018f22c1a6d128a292831e39abf2154a48b5c
Inter <companionpony88@gmail.com>
2020-02-01 23:35:00 +0800

    tests

--- lsapp/resources/views/welcome.blade.php
+++ lsapp/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Salmon
                 </div>
 
                 <div class="links">
